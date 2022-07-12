34a4a9121610aeaf00fb0a45b30150133a9ba0f9
Craig - qrayg <craig@craigerskine.com>
2022-07-11 21:01:28 -0500

    ...

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy('src/_redirects');
   eleventyConfig.addPassthroughCopy('src/_assets');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
@@ -14,8 +17,11 @@ module.exports = function(eleventyConfig) {
     });
     return pageArr.slice(0, limit);
   });
+  eleventyConfig.addFilter("pluck", function (arr, value, attr) {
+    return arr.filter((item) => item[attr] === value);
+  });
   // for zine in (magazines | flatMap('series') | unique('series'))
-  eleventyConfig.addFilter('flatMap', (list, key) => list.flatMap((x) => x[key]))
+  eleventyConfig.addFilter('flatMap', (list, key) => list.flatMap((x) => x[key]));
   eleventyConfig.addFilter('unique', (list, key) => {
     const map = new Map(list.map((x) => [x[key], x]))
     return [...map.values()]
