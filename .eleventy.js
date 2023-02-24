a87780dd473e5a646d747905cf0a9a6f9cff1948
Craig - qrayg <craig@craigerskine.com>
2023-02-24 10:46:23 -0600

    11ty 2.0, esbuild

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,14 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const esbuild = require("esbuild");
 const markdownIt = require("markdown-it");
 const markdownItAttrs = require("markdown-it-attrs");
 const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy('_site/_admin');
-  eleventyConfig.addPassthroughCopy('_site/_redirects');
-  eleventyConfig.addPassthroughCopy('_site/_assets');
-  eleventyConfig.addPassthroughCopy('_site/*.ico');
-  eleventyConfig.addPassthroughCopy('_site/*.png');
-  eleventyConfig.addPassthroughCopy('_site/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('_site/_assets/img');
+  eleventyConfig.addPassthroughCopy({'_site/_assets/root': './'});
   
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -22,6 +19,7 @@ module.exports = function(eleventyConfig) {
   let markdownLibrary = markdownIt().disable('code').use(markdownItAttrs);
   eleventyConfig.setLibrary('md', markdownLibrary);
 
+  // yaml
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
 
   // shortcodes
@@ -35,7 +33,7 @@ module.exports = function(eleventyConfig) {
     this.page.setblock[name] = content;
     return '';
   });
-  
+
   // | randomLimit(6, page.url)
   eleventyConfig.addFilter('randomLimit', (arr, limit, currPage) => {
     const pageArr = arr.filter((page) => page.url !== currPage);
@@ -44,15 +42,30 @@ module.exports = function(eleventyConfig) {
     });
     return pageArr.slice(0, limit);
   });
+
+  // pluck
   eleventyConfig.addFilter('pluck', function (arr, value, attr) {
     return arr.filter((item) => item[attr] === value);
   });
+
   // for item in (items | flatMap('category') | unique('category'))
   eleventyConfig.addFilter('flatMap', (list, key) => list.flatMap((x) => x[key]));
   eleventyConfig.addFilter('unique', (list, key) => {
     const map = new Map(list.map((x) => [x[key], x]))
     return [...map.values()]
   });
+
+  // esbuild
+  eleventyConfig.on('eleventy.before', async () => {
+    await esbuild.build({
+      entryPoints: ['_site/_app/_app.js'],
+      outfile: 'public/_assets/js/_app.js',
+      bundle: true,
+      minify: true,
+      sourcemap: false,
+    });
+  });
+
   return {
     jsDataFileSuffix: '.data',
     markdownTemplateEngine: 'njk',
---
+++ _site/_app/_app.js
@@ -0,0 +1,38 @@
+import 'instant.page';
+
+import { install, injectGlobal } from '@twind/core';
+import presetAutoprefix from '@twind/preset-autoprefix';
+import presetTailwind from '@twind/preset-tailwind';
+
+install({
+  presets: [presetAutoprefix(), presetTailwind()],
+  darkMode: 'class',
+  hash: false,
+  theme: {
+    extend: {
+      colors: ({ theme }) => ({
+        pri: theme('colors.slate'),
+        sec: theme('colors.sky'),
+      }),
+      fontFamily: ({ theme }) => ({
+        sans: 'Outfit,'+ theme('fontFamily.sans'),
+      }),
+    },
+  },
+  rules: [
+    [ 'writing-vertical-rl', { 'writing-mode': 'vertical-rl' } ],
+    [ 'bg-grid', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 4 4\' width=\'4\' height=\'4\'><rect x=\'0\' y=\'0\' width=\'2\' height=\'2\' fill=\'rgba(5,5,5,.33)\'></rect></svg>")', } ],
+  ],
+});
+
+injectGlobal({
+  // global
+  '[x-cloak]': { '@apply': 'hidden', },
+  '.label': { '@apply': 'pb-1 border(b-2 current)' },
+  '.label:before': { '@apply': 'mr-2 pb-1 text([11px] current) tracking-normal leading-none font-bold uppercase', 'content': 'attr(data-label)' },
+});
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
+Alpine.start();
