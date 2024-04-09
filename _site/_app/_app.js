e5df5ec2fb88d090a5697246dfc4c34018f27902
Craig @craigerskine @qrayg <craig@craigerskine.com>
2024-04-09 08:10:55 -0500

    search getting closer

--- _site/_app/_app.js
+++ _site/_app/_app.js
@@ -50,6 +50,7 @@ install({
   },
   rules: [
     [ 'bg-grid', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 6 6\' width=\'6\' height=\'6\' fill=\'rgba(128,128,128,.5)\'><path d=\'m0 0h1v1h1v1h1v1h1v-1h1v-1h1v1h-1v1h-1v1h1v1h1v1h-1v-1h-1v-1h-1v1h-1v1h-1v-1h1v-1h1v-1h-1v-1h-1v-1h-1z\' /></svg>")', } ],
+    [ 'bg-close', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' width=\'1em\' height=\'1em\' fill=\'rgba(128,128,128,1)\'><g><path d=\'M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z\' /></g></svg>")', } ],
   ],
 });
 
