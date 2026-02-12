099c442683b0a75610e2f33126de61c9c03f2e66
Craig @craigerskine @qrayg <craig@craigerskine.com>
2026-02-12 07:56:32 -0600

    removed 11ty img because I already optimize my images

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,4 +1,3 @@
-import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
 import { EleventyRenderPlugin } from '@11ty/eleventy';
 import eleventyNavigationPlugin from '@11ty/eleventy-navigation';
 import esbuild from 'esbuild';
@@ -21,14 +20,6 @@ export default function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget('./_site/_app/_app.js');
 
-  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-    formats: ['auto'],
-    imgAttributes: {
-			alt : '',
-			loading: 'lazy',
-			decoding: 'async',
-		},
-  });
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
